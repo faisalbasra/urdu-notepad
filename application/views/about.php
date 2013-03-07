4fd2a2dff212a2b7c7bd9c67895b49093a336e44
Opensource Pakistan <waqas@opensource.com.pk>
2013-03-07 15:25:54 +0500

    Shifted fonts to urdunotepad.com

--- application/views/about.php
+++ application/views/about.php
@@ -1,8 +1,11 @@
 <?php $this->load->view("defaults/header");?>
-
+<style>
+h1 a {color:#ef303e;}
+h1 a:hover {color:#888;}
+</style>
 <div style="margin:30px;line-height:35px;">
 <h1>Download Font<h1>
-<a style="color:#ef303e;" href="http://opensource.com.pk/Jameel Noori Nastaleeq.ttf" title="Download Font">Please download this font and place it inside your '<span style="color:#333">C:\Windows\Fonts\</span>' folder. And thats all, you are ready to go!</a>
+<a href="http://urdunotepad.com/static/fonts/JameelNooriNastaleeq.ttf" title="Download Font">Please download this font and place it inside your '<span style="color:#333">C:\Windows\Fonts\</span>' folder. And thats all, you are ready to go!</a>
 <h3>If you are using Windows 7 or 8, <a href="http://www.microsoft.com/typography/truetypeinstall.mspx">this is how you install a font on it</a>. It's pretty simple.</h3>
 <h1>About Us</h1>
 <p>Urdu Notepad was stitched together by <a href="http://opensource.com.pk/waqas/" alt="Check out his profile">Waqas Ahmed</a> who is a partner at <a href="http://opensource.com.pk/"> Opensource Pakistan.</a></p>
--- application/views/defaults/header.php
+++ application/views/defaults/header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
- 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <?php
 $page_title = '';
 if(isset($title))
@@ -9,19 +9,20 @@ if(isset($title))
 $page_title .= $this->config->item('site_name');
 ?>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-	<head>
-		<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-	<meta property="og:site_name" content="Urdu Notepad" />
-	<meta property="og:type" content="website" />
-	<meta property="og:locale" content="UR" />
-	<meta property="fb:app_id" content="459117500798602" />
-	<meta property="og:title" content="<?php echo $title; ?>" />
-	<meta property="og:description" content=" مصنف - <?php echo $name; ?> - Read, write Urdu at UrduNotepad.com" />
-	<meta property="og:image" content="http://opensource.com.pk/urdunotepad-logo.png"/>
+    <head>
+        <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    <meta property="og:site_name" content="Urdu Notepad" />
+    <meta property="og:type" content="website" />
+    <meta property="og:locale" content="UR" />
+    <meta property="fb:app_id" content="459117500798602" />
+    <meta property="og:title" content="<?php echo $title; ?>" />
+    <meta property="og:description" content=" مصنف - <?php echo $name; ?> - Read, write Urdu at UrduNotepad.com" />
+    <meta property="og:image" content="http://opensource.com.pk/urdunotepad-logo.png"/>
+<meta name="google-site-verification" content="heZP7OQp99uSlU-I7jFecsJtJm7RJvlFN1Un-tK24vs" />
 <META NAME="Description" CONTENT="<?php echo $title; ?> - <?php echo $name; ?> | Urdu Notepad - Read, Write Urdu Online. Best online Urdu typing tool. Write Urdu on the web. Urdu text unicode writing. Write and save text online. ">
 <META NAME="Title" CONTENT="<?php echo $page_title; ?>">
-		<title><?php echo $page_title; ?></title>
+        <title><?php echo $page_title; ?></title>
 
 <script type="text/javascript" src="https://www.google.com/jsapi"></script>
     <script type="text/javascript">
@@ -49,12 +50,12 @@ $page_title .= $this->config->item('site_name');
         // Enable transliteration in the textbox with id
         // 'transliterateTextarea'.
 
-	control.makeTransliteratable(['gsc-i-id1']);
+    control.makeTransliteratable(['gsc-i-id1']);
         control.makeTransliteratable(['code']);
-	control.makeTransliteratable(['title']);
-	control.makeTransliteratable(['name']);
-	control.makeTransliteratable(['u_0_8']);
-	control.makeTransliteratable(['nicEdit-main']);  
+    control.makeTransliteratable(['title']);
+    control.makeTransliteratable(['name']);
+    control.makeTransliteratable(['u_0_8']);
+    control.makeTransliteratable(['nicEdit-main']);  
       }
       google.setOnLoadCallback(onLoad);
 
@@ -67,9 +68,9 @@ $this->carabiner->config(array(
     'script_dir' => 'static/js/', 
     'style_dir'  => 'static/styles/',
     'cache_dir'  => 'static/asset/',
-    'base_uri'	 => base_url(),
-    'combine'	 => true,
-    'dev'		 => !$this->config->item('combine_assets'),
+    'base_uri'   => base_url(),
+    'combine'    => true,
+    'dev'        => !$this->config->item('combine_assets'),
 ));
 
 // CSS
@@ -86,10 +87,10 @@ $this->carabiner->display('css');
 <style>
 @font-face {
     font-family: NafeesWeb;
-    src: url(http://opensource.com.pk/NafeesWeb.ttf); /*Please change this with your own font path, we might remove it any time*/
+    src: url(http://urdunotepad.com/static/fonts/NafeesWeb.ttf);
 }
-	textarea#code
-		   {font-family:"Jameel Noori Nastaleeq",NafeesWeb !important;font-size:1.4em !important;}
+    textarea#code
+           {font-family:"Jameel Noori Nastaleeq",NafeesWeb !important;font-size:1.4em !important;}
 
 body {font-family:"Jameel Noori Nastaleeq",NafeesWeb !important;font-size:1.4em !important;}
 
@@ -124,25 +125,25 @@ cse .gs-result .gs-snippet, .gs-result .gs-snippet, .cse .gs-spelling-original, 
 }
 </style>
     <script type="text/javascript" src="https://www.google.com/jsapi"></script>
-	<script type="text/javascript">
-	//<![CDATA[
-	var base_url = '<?php echo base_url(); ?>';
-	//]]>
-	</script>
-	
-	</head>
-	<body>
-		<div id="container">
-			<div class="container">			
-				<div class="header">
-					<h1><a href="<?php echo base_url(); ?>" class="title" style="font-family:Jameel Norri Nastaleeq,NafeesWeb;">Urdu Notepad</a></h1>
-					<ul class="tabs">
-						<?php $l = $this->uri->segment(1)?>
-						<?php if(! $this->config->item('private_only')){ ?>
-						<li><a <?php if($l == "lists"){ echo 'class="active"'; }?> href="<?php echo site_url('lists'); ?>" title="Recent Notes">تازہ ترین</a></li>
-						<li><a <?php if($l == "trends"){ echo 'class="active"'; }?> href="<?php echo site_url('trends'); ?>" title="Trending Notes">بہترین</a></li>
+    <script type="text/javascript">
+    //<![CDATA[
+    var base_url = '<?php echo base_url(); ?>';
+    //]]>
+    </script>
+    
+    </head>
+    <body>
+        <div id="container">
+            <div class="container">         
+                <div class="header">
+                    <h1><a href="<?php echo base_url(); ?>" class="title" style="font-family:Jameel Norri Nastaleeq,NafeesWeb;">Urdu Notepad</a></h1>
+                    <ul class="tabs">
+                        <?php $l = $this->uri->segment(1)?>
+                        <?php if(! $this->config->item('private_only')){ ?>
+                        <li><a <?php if($l == "lists"){ echo 'class="active"'; }?> href="<?php echo site_url('lists'); ?>" title="Recent Notes">تازہ ترین</a></li>
+                        <li><a <?php if($l == "trends"){ echo 'class="active"'; }?> href="<?php echo site_url('trends'); ?>" title="Trending Notes">بہترین</a></li>
 <?php } ?>
-						<li><a  <?php if($l == "about"){ echo 'class="active"'; }?> href="<?php echo site_url('about'); ?>" title="About">ہمارے بارے میں</a></li>
+                        <li><a  <?php if($l == "about"){ echo 'class="active"'; }?> href="<?php echo site_url('about'); ?>" title="About">ہمارے بارے میں</a></li>
 <li><a <?php if($l == ""){ echo 'class="active"'; }?> href="<?php echo base_url()?>" title="Create A New Note">نئی تحریر</a></li>
                         <?php
                             if ($this->config->item('require_auth') ){
@@ -151,9 +152,9 @@ cse .gs-result .gs-snippet, .gs-result .gs-snippet, .cse .gs-spelling-original, 
                                 }
                             }
                         ?>
-					</ul>
-				</div>
-				<div class="content">
+                    </ul>
+                </div>
+                <div class="content">
 <!-- AddThis Button BEGIN -->
 <div class="addthis_toolbox addthis_default_style addthis_32x32_style">
 <a class="addthis_button_preferred_1"></a>
@@ -165,15 +166,15 @@ cse .gs-result .gs-snippet, .gs-result .gs-snippet, .cse .gs-spelling-original, 
 </div>
 <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-50b1b4a04d210096"></script>
 <!-- AddThis Button END -->
-					<div class="container">
-						<?php if(isset($status_message)){?>
-						<div class="message success change">
-							<div class="container">
-								<?php echo $status_message; ?>
+                    <div class="container">
+                        <?php if(isset($status_message)){?>
+                        <div class="message success change">
+                            <div class="container">
+                                <?php echo $status_message; ?>
 
-							</div>
-						</div>
-						<?php }?>	
+                            </div>
+                        </div>
+                        <?php }?>   
     
 <script>
   (function() {
